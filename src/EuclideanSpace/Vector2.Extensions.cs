e302672a04e053d8874599bd4d5dd39f0267dbb2
Viktor Ptitselov <qbit86@gmail.com>
2024-08-04 03:26:55 +0300

    Split extension methods of Vector2<TScalar> to separate part

---
+++ src/EuclideanSpace/Vector2.Extensions.cs
@@ -0,0 +1,58 @@
+namespace EuclideanSpace
+{
+    using System.Diagnostics;
+    using System.Numerics;
+    using System.Runtime.CompilerServices;
+
+    public static partial class Vector2
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TScalar Length<TScalar>(this Vector2<TScalar> value)
+            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
+            IMultiplyOperators<TScalar, TScalar, TScalar>,
+            ISubtractionOperators<TScalar, TScalar, TScalar>,
+            IUnaryNegationOperators<TScalar, TScalar>,
+            IDivisionOperators<TScalar, TScalar, TScalar>,
+            IRootFunctions<TScalar>
+        {
+            var lengthSquared = value.LengthSquared();
+            return TScalar.Sqrt(lengthSquared);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2<TScalar> Normalize<TScalar>(this Vector2<TScalar> value)
+            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
+            IMultiplyOperators<TScalar, TScalar, TScalar>,
+            ISubtractionOperators<TScalar, TScalar, TScalar>,
+            IUnaryNegationOperators<TScalar, TScalar>,
+            IDivisionOperators<TScalar, TScalar, TScalar>,
+            IRootFunctions<TScalar>
+            => value / value.Length();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static TScalar GetElementUnsafe<TScalar>(in this Vector2<TScalar> vector, int index)
+            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
+            IMultiplyOperators<TScalar, TScalar, TScalar>,
+            ISubtractionOperators<TScalar, TScalar, TScalar>,
+            IUnaryNegationOperators<TScalar, TScalar>,
+            IDivisionOperators<TScalar, TScalar, TScalar>
+        {
+            Debug.Assert(index is >= 0 and < Count);
+            ref var address = ref Unsafe.AsRef(in vector._x);
+            return Unsafe.Add(ref address, index);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static TScalar GetElement<TScalar>(this Vector2<TScalar> vector, int index)
+            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
+            IMultiplyOperators<TScalar, TScalar, TScalar>,
+            ISubtractionOperators<TScalar, TScalar, TScalar>,
+            IUnaryNegationOperators<TScalar, TScalar>,
+            IDivisionOperators<TScalar, TScalar, TScalar>
+        {
+            ThrowHelpers.ThrowIfGreaterThanOrEqual((uint)index, (uint)Count);
+
+            return vector.GetElementUnsafe(index);
+        }
+    }
+}
--- src/EuclideanSpace/Vector2.cs
+++ src/EuclideanSpace/Vector2.cs
@@ -1,11 +1,10 @@
 namespace EuclideanSpace
 {
     using System;
-    using System.Diagnostics;
     using System.Numerics;
     using System.Runtime.CompilerServices;
 
-    public static class Vector2
+    public static partial class Vector2
     {
         private const int Count = 2;
 
@@ -165,29 +164,6 @@ namespace EuclideanSpace
             => left.X * right.Y - left.Y * right.X;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static TScalar Length<TScalar>(this Vector2<TScalar> value)
-            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
-            IMultiplyOperators<TScalar, TScalar, TScalar>,
-            ISubtractionOperators<TScalar, TScalar, TScalar>,
-            IUnaryNegationOperators<TScalar, TScalar>,
-            IDivisionOperators<TScalar, TScalar, TScalar>,
-            IRootFunctions<TScalar>
-        {
-            var lengthSquared = value.LengthSquared();
-            return TScalar.Sqrt(lengthSquared);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2<TScalar> Normalize<TScalar>(this Vector2<TScalar> value)
-            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
-            IMultiplyOperators<TScalar, TScalar, TScalar>,
-            ISubtractionOperators<TScalar, TScalar, TScalar>,
-            IUnaryNegationOperators<TScalar, TScalar>,
-            IDivisionOperators<TScalar, TScalar, TScalar>,
-            IRootFunctions<TScalar>
-            => value / value.Length();
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TScalar DistanceSquared<TScalar>(Vector2<TScalar> value1, Vector2<TScalar> value2)
             where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
             IMultiplyOperators<TScalar, TScalar, TScalar>,
@@ -215,31 +191,5 @@ namespace EuclideanSpace
             IDivisionOperators<TScalar, TScalar, TScalar>,
             IMultiplicativeIdentity<TScalar, TScalar>
             => left * (TScalar.MultiplicativeIdentity - amount) + right * amount;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static TScalar GetElement<TScalar>(this Vector2<TScalar> vector, int index)
-            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
-            IMultiplyOperators<TScalar, TScalar, TScalar>,
-            ISubtractionOperators<TScalar, TScalar, TScalar>,
-            IUnaryNegationOperators<TScalar, TScalar>,
-            IDivisionOperators<TScalar, TScalar, TScalar>
-        {
-            ThrowHelpers.ThrowIfGreaterThanOrEqual((uint)index, (uint)Count);
-
-            return vector.GetElementUnsafe(index);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static TScalar GetElementUnsafe<TScalar>(in this Vector2<TScalar> vector, int index)
-            where TScalar : IAdditionOperators<TScalar, TScalar, TScalar>,
-            IMultiplyOperators<TScalar, TScalar, TScalar>,
-            ISubtractionOperators<TScalar, TScalar, TScalar>,
-            IUnaryNegationOperators<TScalar, TScalar>,
-            IDivisionOperators<TScalar, TScalar, TScalar>
-        {
-            Debug.Assert(index is >= 0 and < Count);
-            ref var address = ref Unsafe.AsRef(in vector._x);
-            return Unsafe.Add(ref address, index);
-        }
     }
 }
