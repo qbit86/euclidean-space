55999c6bc33b99560e77108c13c5ece6b95a952f
Viktor Ptitselov <qbit86@gmail.com>
2024-09-08 02:47:39 +0300

    Add XML-doc comments for `Point*_1`

--- src/EuclideanSpace/Point3_1.cs
+++ src/EuclideanSpace/Point3_1.cs
@@ -4,6 +4,10 @@ namespace EuclideanSpace
     using System.Numerics;
     using System.Runtime.CompilerServices;
 
+    /// <summary>
+    /// Represents a point with three scalar components.
+    /// </summary>
+    /// <typeparam name="TScalar">The type of the components of the point.</typeparam>
     public readonly partial struct Point3<TScalar> :
         IEquatable<Point3<TScalar>>,
         IFormattable,
@@ -22,8 +26,18 @@ namespace EuclideanSpace
         private readonly TScalar _y;
         private readonly TScalar _z;
 
+        /// <summary>
+        /// Creates a new <see cref="Point3{T}" /> instance whose components have the same value.
+        /// </summary>
+        /// <param name="value">The value to assign to all components.</param>
         public Point3(TScalar value) : this(value, value, value) { }
 
+        /// <summary>
+        /// Creates a point whose components have the specified values.
+        /// </summary>
+        /// <param name="x">The value to assign to the <see cref="X" /> component.</param>
+        /// <param name="y">The value to assign to the <see cref="Y" /> component.</param>
+        /// <param name="z">The value to assign to the <see cref="Z" /> component.</param>
         public Point3(TScalar x, TScalar y, TScalar z)
         {
             _x = x;
@@ -31,36 +45,50 @@ namespace EuclideanSpace
             _z = z;
         }
 
-        public Point3(ReadOnlySpan<TScalar> elements)
+        /// <summary>
+        /// Constructs a point from the given <see cref="ReadOnlySpan{TScalar}" />. The span must contain at least 3 elements.
+        /// </summary>
+        /// <param name="values">The span of elements to assign to the point.</param>
+        public Point3(ReadOnlySpan<TScalar> values)
         {
-            if (elements.Length < Point3.Count)
-                ThrowHelpers.ThrowArgumentOutOfRangeException(nameof(elements));
+            if (values.Length < Point3.Count)
+                ThrowHelpers.ThrowArgumentOutOfRangeException(nameof(values));
 
-            _x = elements[0];
-            _y = elements[1];
-            _z = elements[2];
+            _x = values[0];
+            _y = values[1];
+            _z = values[2];
         }
 
+        ///<summary>The X component of the point.</summary>
         public TScalar X => _x;
 
+        ///<summary>The Y component of the point.</summary>
         public TScalar Y => _y;
 
+        ///<summary>The Z component of the point.</summary>
         public TScalar Z => _z;
 
+        /// <summary>Gets the element at the specified index.</summary>
+        /// <param name="index">The index of the element to get.</param>
+        /// <returns>The element at <paramref name="index" />.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index" /> was less than zero or greater than the number of elements.</exception>
         public TScalar this[int index]
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => this.GetElement(index);
         }
 
+        /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Point3<TScalar> operator +(Point3<TScalar> point, Vector3<TScalar> vector) =>
             new(point.X + vector.X, point.Y + vector.Y, point.Z + vector.Z);
 
+        /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3<TScalar> operator -(Point3<TScalar> left, Point3<TScalar> right) =>
             new(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
 
+        /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Point3<TScalar> operator -(Point3<TScalar> left, Vector3<TScalar> right) =>
             new(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
