9732a9f8ff90bf2832973b7d8d0f91bb42258460
Viktor Ptitselov <qbit86@gmail.com>
2024-09-09 02:33:35 +0300

    Add XML-doc comments for `Point*_1` trivia

--- src/EuclideanSpace/Point3_1.Trivia.cs
+++ src/EuclideanSpace/Point3_1.Trivia.cs
@@ -7,27 +7,35 @@ namespace EuclideanSpace
 
     public readonly partial struct Point3<TScalar>
     {
+        /// <inheritdoc />
         public string ToString(string? format, IFormatProvider? formatProvider)
         {
             FormattableString formattable = $"<{_x}, {_y}, {_z}>";
             return formattable.ToString(formatProvider);
         }
 
+        /// <summary>Returns the string representation of the current instance using default formatting.</summary>
+        /// <returns>The string representation of the current instance.</returns>
         public override string ToString() => ToString("G", CultureInfo.InvariantCulture);
 
+        /// <inheritdoc />
         public bool Equals(Point3<TScalar> other)
         {
             var c = EqualityComparer<TScalar>.Default;
             return c.Equals(_x, other._x) && c.Equals(_y, other._y) && c.Equals(_z, other._z);
         }
 
+        /// <inheritdoc />
         public override bool Equals(object? obj) => obj is Point3<TScalar> other && Equals(other);
 
+        /// <inheritdoc />
         public override int GetHashCode() => HashCode.Combine(_x, _y, _z);
 
+        /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(Point3<TScalar> left, Point3<TScalar> right) => left.Equals(right);
 
+        /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(Point3<TScalar> left, Point3<TScalar> right) => !left.Equals(right);
     }
